786c65196b6a764112328d00ced25a89c0aa93d9
Elvo <elvisbitolo11@gmail.com>
2026-03-03 11:47:10 +0300

    Add files via upload

---
+++ el.py
@@ -0,0 +1,16 @@
+#python expression for the sum of 10 and 3
+print(10 + 3)
+#python expression for the difference of 15  and 7
+print(15 - 7)
+#python expression for the product of 4 and 8
+print(4 * 8)
+# dividing 18 by 3
+print(18//3)
+#findiing float through division of 11 and 4
+print(11/4)
+ #remainder of 9 divided by 4
+print(9%4)
+#raise 6 to the power of 3
+print(6**3)
+#negationoperator making 12 negative
+print(-12)
